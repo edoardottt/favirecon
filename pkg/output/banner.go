4dc5d7833c712432c82094ccf5b443196dad7634
edoardottt <edoardott@gmail.com>
2023-03-29 09:22:00 +0200

    v0.0.4

--- pkg/output/banner.go
+++ pkg/output/banner.go
@@ -12,7 +12,7 @@ import "github.com/projectdiscovery/gologger"
 var printed = false
 
 const (
-	Version = "v0.0.3"
+	Version = "v0.0.4"
 	banner  = `    ____            _                          
    / __/___  __  __(_)_______  _________  ____ 
   / /_/ __ ` + `\/ | / / / ___/ _ \/ ___/ __ \/ __ \
